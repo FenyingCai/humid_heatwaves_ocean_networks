0f7719a58b08cfc6b93bc9c9272925ec2e3e2782
FenyingCai <fenyingcai2023@163.com>
2025-12-24 16:02:25 +0100

    Add files via upload

---
+++ 2_networks/part2_wbt95_sst95_SH/2_yr42day90_significant190.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+
+##--##--##--##--     --##--##--##--##
+i = 190
+j = 190
+random_syn = np.zeros(1000, dtype=float)   ## 重复 1000 次
+for ktimes in range(1000):   ## 重复 1000 次
+  PC1 = np.zeros(90*42, dtype=float)
+  PC2 = np.zeros(90*42, dtype=float)
+  PC1[0:i]=1.0    ## PC1 有 i 天发生事件
+  PC2[0:j]=1.0    ## PC2 有 j 天发生事件
+  np.random.shuffle(PC1)
+  np.random.shuffle(PC2)
+  random_syn[ktimes] = np.sum(np.array(PC1+PC2==2), axis=0)
+
+matrix99 = np.nanpercentile(random_syn,99)
+print("  99 th  =  ", matrix99)  ## 17
+    
+
